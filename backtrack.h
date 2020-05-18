68c396b53ed75a8902859952dd9753da8232f34d
Kun Hwi Ko <khk305@stern.nyu.edu>
2020-05-18 02:24:23 -0400

    add backtracking

--- backtrack.h
+++ backtrack.h
@@ -54,4 +54,23 @@ class Backtrack{
 
         return true;
     }
+
+    bool backtracking(){
+        tuple<int,int> find_coord = find_zero();
+
+        // once base case is reached, end the backtracking
+        if (get<0>(find_coord) == -1)
+            return true;
+
+        int row = get<0>(find_coord);
+        int col = get<1>(find_coord);
+        for (int i = 1; i < 10; i++){
+            if (valid(i, find_coord) == true){
+                board[row][col] = i;
+                if (backtracking() == true) return true;
+                board[row][col] = 0;
+            }
+        }
+        return false;
+    }
 };
