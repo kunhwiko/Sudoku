d02f1b9634ec6d0e3e3b2c4e8ae1766e4b208c74
Kun Hwi Ko <khk305@stern.nyu.edu>
2020-05-27 00:12:32 -0400

    initialize board

--- GUI/mainwindow.cpp
+++ GUI/mainwindow.cpp
@@ -6,12 +6,19 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    //createpuzzle* cp = new createpuzzle();
+    createpuzzle* cp = new createpuzzle();
+
+    // initialize the board
     for(int i = 0; i < 9; i++){
         for(int j = 0; j < 9; j++){
-            //QTableWidgetItem* Cell = ui->sudoku_board->item(i,j);
+            QTableWidgetItem* square = ui->sudoku_board->item(i,j);
+            QString str = "";
+            str += '0' + cp->get_matrix()[i][j];
+            square -> setText(str);
         }
     }
+
+    QWidget::setWindowTitle("Sudoku Game");
 }
 
 MainWindow::~MainWindow()
